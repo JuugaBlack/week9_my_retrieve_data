c51e9e460dbf30a5765a049ffd717c3f55c0f531
ZHR <1609354709@qq.com>
2024-04-23 21:16:19 +0800

    week9

--- fetch_data/bin/fetch_data.dart
+++ fetch_data/bin/fetch_data.dart
@@ -1,6 +1,79 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:http/retry.dart';
+
+class PackageInfo {
+  final String name;
+  final String latestVersion;
+  final String description;
+  final String publisher;
+  final Uri? repository;
+
+  PackageInfo({
+    required this.name,
+    required this.latestVersion,
+    required this.description,
+    required this.publisher,
+    this.repository,
+  });
+
+  factory PackageInfo.fromJson(Map<String, dynamic> json) {
+    final repository = json['repository'] as String?;
+
+    return PackageInfo(
+      name: json['name'] as String,
+      latestVersion: json['latestVersion'] as String,
+      description: json['description'] as String,
+      publisher: json['publisher'] as String,
+      repository: repository != null ? Uri.tryParse(repository) : null,
+    );
+  }
+}
 
 void main() async {
   final httpPackageUrl = Uri.https('dart.dev', '/f/packages/http.json');
   final httpPackageInfo = await http.read(httpPackageUrl);
+  final httpPackageResponse = await http.get(httpPackageUrl);
+  if (httpPackageResponse.statusCode != 200) {
+    print('Failed to retrieve the http package!');
+    return;
+  }
+  print(httpPackageInfo);
+  print(httpPackageResponse.body);
+
+  final client = RetryClient(http.Client());
+  try {
+    final httpPackageInfo = await client.read(httpPackageUrl);
+    print(httpPackageInfo);
+  } finally {
+    client.close();
+  }
+
+  final httpPackageJson = json.decode(httpPackageInfo) as Map<String, dynamic>;
+  print(httpPackageJson);
+}
+
+Future<PackageInfo> getPackage(String packageName) async {
+  final packageUrl = Uri.https('dart.dev', '/f/packages/$packageName.json');
+  final packageResponse = await http.get(packageUrl);
+
+  // If the request didn't succeed, throw an exception
+  if (packageResponse.statusCode != 200) {
+    throw PackageRetrievalException(
+      packageName: packageName,
+      statusCode: packageResponse.statusCode,
+    );
+  }
+
+  final packageJson = json.decode(packageResponse.body) as Map<String, dynamic>;
+
+  return PackageInfo.fromJson(packageJson);
+}
+
+class PackageRetrievalException implements Exception {
+  final String packageName;
+  final int? statusCode;
+
+  PackageRetrievalException({required this.packageName, this.statusCode});
 }
